d35c7d69aa824fba55149981f34489f698845f1a
Fola Flor <dpcalfola@gmail.com>
2022-01-17 18:11:49 +0900

    ver 0.03
    adjust font size, layout for any iPhone device
    
    connect buttons to ViewController

--- RockPaperScissorsGame/ViewController.swift
+++ RockPaperScissorsGame/ViewController.swift
@@ -9,23 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var mainLable: UILabel!
 
 
 	@IBOutlet weak var comImageView: UIImageView!
 	@IBOutlet weak var userImageView: UIImageView!
 
+	@IBOutlet weak var comChoiceLabel: UILabel!
+	@IBOutlet weak var userChoiceLabel: UILabel!
+
+
+
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		// 이미지 삽입: #imageLiteral() 또는 UIImage(named: 파일명)
 		comImageView.image = #imageLiteral(resourceName: "readyLeft")
-		userImageView.image = #imageLiteral(resourceName: "readyRight")
+		userImageView.image = UIImage(named: "readyRight.png")
 
-		// Do any additional setup after loading the view.
+		comChoiceLabel.text = "Ready"
+		userChoiceLabel.text = "Ready"
+
 	}
 
 
+	@IBAction func rpsButtonTapped(_ sender: UIButton) {
 
 
+	}
+
+
+	@IBAction func selectButtonTapped(_ sender: UIButton) {
+
+
+	}
+
+
+	@IBAction func resetButtonTapped(_ sender: UIButton) {
+	}
+	
+	
 
 
 }
